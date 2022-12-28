9a0a7b25d9b0a4b929fb733a91e946712a70d0c7
elviric <santu0002@gmail.com>
2022-12-28 16:49:16 +0530

    nftfi + next-config-js webpack fix

--- pages/borrow.js
+++ pages/borrow.js
@@ -1,6 +1,43 @@
 import React from "react";
+import NFTfi from "@nftfi/js";
+import dotenv from 'dotenv';
+dotenv.config();
 
+async function run() {
+  // Init the NFTfi SDK
+  const nftfi = await NFTfi.init({
+    config: { api: { key: process.env.NFTFI_SDK_API_KEY } },
+    ethereum: {
+      account: { privateKey: process.env.NFTFI_SDK_ETHEREUM_LENDER_ACCOUNT_PRIVATE_KEY },
+      provider: { url: process.env.NFTFI_SDK_ETHEREUM_PROVIDER_URL }
+    }
+  });
+  // Get listings
+  const listings = await nftfi.listings.get({
+    filters: {
+      nftAddresses: []
+    },
+    pagination: {
+      limit: 20,
+      page: 1
+    }
+  });
+  console.log(`[INFO] found ${listings.length} listing(s).`);
+  // Proceed if we find listings
+  if (listings.length > 0) {
+    for (var i = 0; i < listings.length; i++) {
+      const listing = listings[i];
+      console.log(`[INFO] listing #${i + 1}: ${JSON.stringify(listing)}`);
+    }
+  }
+}
+
+run().catch(error => {
+  console.error(error);
+  process.exitCode = 1;
+});
 const borrow = () => {
+  
   return (
     <div className="mx-20 mt-1 flex">
       <h1>Borrow</h1>
